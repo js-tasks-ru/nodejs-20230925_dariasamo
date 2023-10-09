563fa8438e3d4e401f072138816b4c61a3086c86
darya.samokhvalova <darya.samokhvalova@phrase.com>
2023-10-09 16:39:30 +0200

    add solution for 3-module 1-task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,21 @@
 const stream = require('stream');
+
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.bufferData = Buffer.alloc(0);
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.bufferData = Buffer.concat([this.bufferData, chunk]);
+    if (this.bufferData.length > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
