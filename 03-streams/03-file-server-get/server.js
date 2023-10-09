515155819d949a5cbcfa31a93b711f24b3d99c4a
darya.samokhvalova <darya.samokhvalova@phrase.com>
2023-10-09 17:59:26 +0200

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,4 +1,4 @@
-const url = require('url');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
@@ -10,9 +10,30 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const handleGetFile = () => {
+    const hasNestedFolders = pathname.includes('/');
+    if (hasNestedFolders) {
+      res.statusCode = 400;
+      res.end('Nested paths are not supported');
+      return;
+    }
+
+    const readStream = fs.createReadStream(filepath);
+    readStream.on('error', (err) => {
+      if (err.code === 'ENOENT') {
+        res.statusCode = 404;
+        res.end('File not found');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal server error');
+      }
+    });
+    readStream.pipe(res);
+  };
+
   switch (req.method) {
     case 'GET':
-
+      handleGetFile();
       break;
 
     default:
