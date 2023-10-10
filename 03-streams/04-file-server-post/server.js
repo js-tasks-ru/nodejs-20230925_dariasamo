bbd00ba2299ab39239203c25c457008d966ad64a
darya.samokhvalova <darya.samokhvalova@phrase.com>
2023-10-10 17:25:33 +0200

    add solution for 3-module 4-task

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,7 @@
-const url = require('url');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -10,9 +11,58 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const handlePostFile = () => {
+    const hasNestedFolders = pathname.includes('/');
+    if (hasNestedFolders) {
+      res.statusCode = 400;
+      res.end('Nested paths are not supported');
+      return;
+    }
+
+    if (fs.existsSync(filepath)) {
+      res.statusCode = 409;
+      res.end('File already exists');
+      return;
+    }
+
+    const limitedStream = new LimitSizeStream({limit: 1000000});
+    const outStream = fs.createWriteStream(filepath);
+
+    limitedStream.pipe(outStream);
+
+    req.on('data', (chunk) => {
+      limitedStream.write(chunk);
+    });
+
+    req.on('end', () => {
+      limitedStream.end();
+    });
+
+    req.on('aborted', () => {
+      limitedStream.end();
+      fs.unlink(filepath, () => {});
+    });
+
+    limitedStream.on('error', (err) => {
+      if (err.code === 'LIMIT_EXCEEDED') {
+        res.statusCode = 413;
+        fs.unlink(filepath, () => {});
+        res.end('Limit exceeded');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal server error');
+      }
+    });
+
+    outStream.on('finish', () => {
+      res.statusCode = 201;
+      res.end('File created successfully');
+    });
+  };
+
   switch (req.method) {
     case 'POST':
-
+      handlePostFile();
       break;
 
     default:
